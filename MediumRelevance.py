a3b29dbcf844e24a9b78e59b6cfe751973b3d530
Satyajeet Pradhan <satyajeet.pradhan@gmail.com>
2020-04-18 22:27:44 -0400

    LocationRelevantCode
    Signed-off-by: Satyajeet Pradhan <satyajeet.pradhan@gmail.com>

---
+++ MediumRelevance.py
@@ -0,0 +1,44 @@
+# Mainly focussed around streaming data on Palo Alto
+
+from TwitterStreaming.twitterAuthModule import twitterAuth as tA
+from TwitterStreaming.gitAuthModule import gitAuth as gA
+
+from tweepy.streaming import StreamListener
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import Cursor
+from tweepy import API
+
+import os
+import datetime
+
+
+#Variable to store output files
+outputPath = '..//SWB//outputMedium//'
+
+#Create a directory if it doesn't exist
+if not os.path.isdir(outputPath):
+    os.mkdir(outputPath)
+
+auth = tA.authorization()
+api = API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+
+#Obtain Geo Code Location of Palo Alto California
+places = api.geo_search(query="USA", granularity="country")
+place_id = places[0].id
+
+preventiveString, riskString, elderlyString, sentiments, misc = gA.returnSearchString()
+
+searchString = 'place:'+place_id+' '+riskString+' OR '+preventiveString
+
+cursor = Cursor(api.search, q=searchString, count=20, lang="en", tweet_mode='extended')
+api = API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+
+maxCount = 1000
+count = 0
+
+for tweet in cursor.items():
+    fileName = "tweets_"+str(datetime.datetime.now().date()).replace('-', '_')
+    file = open(outputPath+fileName+'.txt', 'a')
+    file.write(tweet + '\n')
+    print(tweet)
---
+++ TwitterStreaming/gitAuthModule/gitAuth.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from github import Github
+
+#Function to convert a given data file to bytes
+def bytesToDF(bytesDataFile):
+    from io import StringIO
+    s =str(bytesDataFile,'utf-8')
+    data = StringIO(s)
+    return pd.read_csv(data)
+
+
+
+
+path = "/Users/satyajeetpradhan/Config/"
+
+file = open(path+'Github.txt','r')
+
+c = 0
+
+for line in file:
+    c = c+1
+    if c == 1:
+        username = str(line.partition('=')[2].strip())
+    elif c == 2:
+        password = str(line.partition('=')[2].strip())
+    else:
+        break
+
+g = Github(username, password)
+
+del username, password
+
+repo = g.get_repo("keriwheatley/swb-covid-19-twitter-repo")
+
+contents = repo.get_contents("")
+
+dfHashTags = bytesToDF(repo.get_contents(contents[2].path)[2].decoded_content)
+
+def StringCombine(col_name):
+    stringList = ['#'+x for x in dfHashTags[col_name]]
+    string =''
+    for stringVal in stringList:
+        string = string +' OR '+stringVal
+    return string[3:]
+
+#preventiveList = ['#'+x for x in dfHashTags['preventative_measures']]
+#riskSymptomList = ['#'+x for x in dfHashTags['risks_symptoms']]
+
+preventiveString = StringCombine('preventative_measures')
+riskString = StringCombine('risks_symptoms')
+elderlyString = StringCombine('elderly')
+sentiments = StringCombine('sentiments')
+misc = StringCombine('miscellaneous')
+print(preventiveString)
+print("\n\n")
+print(riskString)
+
+def returnSearchString():
+    return preventiveString, riskString, elderlyString, sentiments, misc
+
+
+
+
+
+
+
