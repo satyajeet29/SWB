5ce092e924aec7332ff9de105a00bb0bdbf2c04d
Satyajeet Pradhan <satyajeet.pradhan@gmail.com>
2020-04-20 23:43:01 -0400

    Fixed counter bug in Shell
    Signed-off-by: Satyajeet Pradhan <satyajeet.pradhan@gmail.com>

--- execPyScript.sh
+++ execPyScript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "$(date '+%Y %b %d %H:%M:%S'):: Started";
-while true
 c = 0
 s = 0
 f =0
+while true
 do
     echo "--->> $(date '+%Y %b %d %H:%M:%S') :: Sequence $c "
     (command python3 twitterCursorDC.py && ((s = s+1)) && echo "Success #: $s") || (((f=f+1))&& echo "Failure #: $f" && continue)
