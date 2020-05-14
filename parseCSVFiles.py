a293e03e5c145184d95adc91f49222fa39ed3741
Satyajeet Pradhan <satyajeet.pradhan@gmail.com>
2020-05-13 21:43:59 -0400

    parseCSVFiles.py
    Signed-off-by: Satyajeet Pradhan <satyajeet.pradhan@gmail.com>

--- parseCSVFiles.py
+++ parseCSVFiles.py
@@ -2,16 +2,20 @@ import re,os,ast,json
 import datetime
 import pandas as pd
 from combineFileUtility import fileParseLogic as fpl
-inputpath ="/home/satyajeet29/SWB_CombineFiles/DataFiles/".replace('/','//')
-outputpath= "/home/satyajeet29/SWB_CombineFiles/outputPath/".replace('/','//')
+#inputpath ="/home/satyajeet29/SWB_CombineFiles/DataFiles/".replace('/','//')
+#outputpath= "/home/satyajeet29/SWB_CombineFiles/outputPath/".replace('/','//')
+inputpath ="/Users/satyajeetpradhan/SWB_Covid_19/DataFiles/".replace('/','//')
+outputpath= "/Users/satyajeetpradhan/SWB_Covid_19/outputPath/".replace('/','//')
+
 #subDirList = ['PaloAlto','USA','WashingtonDC','SunnyVale','set0','set1','set2','set3','set4','set5']
-subDirList = ['PaloAlto']
+subDirList = ['USA','WashingtonDC','set3','set5']
 #obtain files from given subdirectory
 
 for dirVal in subDirList:
     filepath = inputpath+dirVal+'//'
     fileOutPath = outputpath + dirVal + '//'
     fileList =os.listdir(filepath)
+    fileList.sort()
     for file in fileList:
         print(str(datetime.datetime.now()) + " : " + dirVal  +" "+file + " started")
         df, dfuniq = fpl.fileParse(filepath+file)
